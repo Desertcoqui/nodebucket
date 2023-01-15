75440dfd4dca21b0966d4df8abb0ebc599544bf7
desertcoqui <fdetres@my365.bellevue.edu>
2023-01-15 11:49:50 -0600

    auth guard type script complete

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,3 +1,11 @@
+// Title: NodeBucket
+// Author: Professor Krasso
+// Date: Jan 15 2023
+// Modified By: Ferdinand Detres Jr
+// Attributions: https://www.google.com/books/edition/Getting_MEAN_with_Mongo_Express_Angular/sTgzEAAAQBAJ?hl=en&gbpv=1&dq=Getting+MEAN+with+MongoDB,+Express,+Angular,+and+Node+2+nd+Edition%3B+Simon+Holmes+and+Clive+Harber%3B+Manning+Publications+2019+pdf&printsec=frontcover
+// In-Class tutorials
+
+//import statements
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
@@ -7,11 +15,20 @@ import { CookieService } from "ngx-cookie-service";
 @Injectable({
   providedIn: "root",
 })
+
+//using cookie service dependency so it knows when user is logged in, it stores the cookie in the users browser during the sign in process
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private cookieService: CookieService) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    const sessionUser = this.cookieService.get("session_user");
+    if (sessionUser) {
+      return true;
+    } else {
+      this.router.navigate(["/session/login"]);
+      return false;
+    }
   }
 }
--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -5,6 +5,7 @@
 // Attributions: https://www.google.com/books/edition/Getting_MEAN_with_Mongo_Express_Angular/sTgzEAAAQBAJ?hl=en&gbpv=1&dq=Getting+MEAN+with+MongoDB,+Express,+Angular,+and+Node+2+nd+Edition%3B+Simon+Holmes+and+Clive+Harber%3B+Manning+Publications+2019+pdf&printsec=frontcover
 // In-Class tutorials
 
+//import statements
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
@@ -12,9 +13,11 @@ import { Injectable } from "@angular/core";
 @Injectable({
   providedIn: "root",
 })
+
+//this returns the api and matches employees ID
 export class EmployeeService {
   constructor(private http: HttpClient) {}
   findEmployeeById(empId: number): Observable<any> {
-    return this.http.get("/api/employer/" + empId);
+    return this.http.get("/api/employees/" + empId);
   }
 }
