2329a214657bbc37777aa0571bf2437aba92bd04
desertcoqui <fdetres@my365.bellevue.edu>
2023-01-15 12:30:07 -0600

    prime theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,53 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {BaseLayoutComponent} from "./shared/base-layout/base-layout.component";
-import {HomeComponent} from "./pages/home/home.component";
+// Title: NodeBucket
+// Author: Professor Krasso
+// Date: Jan 15 2023
+// Modified By: Ferdinand Detres Jr
+// Attributions: https://www.google.com/books/edition/Getting_MEAN_with_Mongo_Express_Angular/sTgzEAAAQBAJ?hl=en&gbpv=1&dq=Getting+MEAN+with+MongoDB,+Express,+Angular,+and+Node+2+nd+Edition%3B+Simon+Holmes+and+Clive+Harber%3B+Manning+Publications+2019+pdf&printsec=frontcover
+// In-Class tutorials
 
+//import statements
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BaseLayoutComponent } from "./shared/base-layout/base-layout.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AuthGuard } from "./auth.guard";
+import { AuthLayoutComponent } from "./shared/auth-layout/auth-layout.component";
+import { LoginComponent } from "./pages/login/login.component";
+
+//Routes for the auth/ base layout
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: BaseLayoutComponent,
     children: [
       {
-        path: '',
-        component: HomeComponent
-      }
-    ]
-  }
+        path: "",
+        component: HomeComponent,
+        canActivate: [AuthGuard],
+      },
+    ],
+  },
+  {
+    path: "session",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "login",
+        component: LoginComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true, enableTracing: false, scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy'})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      enableTracing: false,
+      scrollPositionRestoration: "enabled",
+      relativeLinkResolution: "legacy",
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,11 @@
+// Title: NodeBucket
+// Author: Professor Krasso
+// Date: Jan 15 2023
+// Modified By: Ferdinand Detres Jr
+// Attributions: https://www.google.com/books/edition/Getting_MEAN_with_Mongo_Express_Angular/sTgzEAAAQBAJ?hl=en&gbpv=1&dq=Getting+MEAN+with+MongoDB,+Express,+Angular,+and+Node+2+nd+Edition%3B+Simon+Holmes+and+Clive+Harber%3B+Manning+Publications+2019+pdf&printsec=frontcover
+// In-Class tutorials
+
+//all the applications dependencies are imported here
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
@@ -14,6 +22,14 @@ import { MatIconModule } from "@angular/material/icon";
 import { LoginComponent } from "./pages/login/login.component";
 import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { CookieService } from "ngx-cookie-service";
+import { MessagesModule } from "primeng/messages";
+import { MessageModule } from "primeng/message";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AuthLayoutComponent, BaseLayoutComponent, LoginComponent],
@@ -26,8 +42,17 @@ import { CookieService } from "ngx-cookie-service";
     MatButtonModule,
     MatIconModule,
     HttpClientModule,
+    MessageModule,
+    MessagesModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
