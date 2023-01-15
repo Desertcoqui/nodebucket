a53ee175a4d3266accebd5bd4169b73fb147d148
desertcoqui <fdetres@my365.bellevue.edu>
2023-01-15 11:16:16 -0600

    employee services ts

--- src/app/shared/models/employee.interface.ts
+++ src/app/shared/models/employee.interface.ts
@@ -1,3 +1,11 @@
+// Title: NodeBucket
+// Author: Professor Krasso
+// Date: Jan 15 2023
+// Modified By: Ferdinand Detres Jr
+// Attributions: https://www.section.io/engineering-education/nodejs-mongoosejs-mongodb/
+// In-Class tutorials
+
+//employee interface with three objects
 export interface Employee {
   empId: number;
   firstName: string;
--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -1,3 +1,10 @@
+// Title: NodeBucket
+// Author: Professor Krasso
+// Date: Jan 15 2023
+// Modified By: Ferdinand Detres Jr
+// Attributions: https://www.google.com/books/edition/Getting_MEAN_with_Mongo_Express_Angular/sTgzEAAAQBAJ?hl=en&gbpv=1&dq=Getting+MEAN+with+MongoDB,+Express,+Angular,+and+Node+2+nd+Edition%3B+Simon+Holmes+and+Clive+Harber%3B+Manning+Publications+2019+pdf&printsec=frontcover
+// In-Class tutorials
+
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
@@ -7,4 +14,7 @@ import { Injectable } from "@angular/core";
 })
 export class EmployeeService {
   constructor(private http: HttpClient) {}
+  findEmployeeById(empId: number): Observable<any> {
+    return this.http.get("/api/employer/" + empId);
+  }
 }
