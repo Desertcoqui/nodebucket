1df1babf3e9cfe51aacf5dddabcc2d5bd732ac21
desertcoqui <fdetres@my365.bellevue.edu>
2023-01-16 18:53:06 -0600

    fixed primeNG carrot issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+// Title: NodeBucket
+// Author: Professor Krasso
+// Date: Jan 15 2023
+// Modified By: Ferdinand Detres Jr
+// Attributions: https://www.google.com/books/edition/Getting_MEAN_with_Mongo_Express_Angular/sTgzEAAAQBAJ?hl=en&gbpv=1&dq=Getting+MEAN+with+MongoDB,+Express,+Angular,+and+Node+2+nd+Edition%3B+Simon+Holmes+and+Clive+Harber%3B+Manning+Publications+2019+pdf&printsec=frontcover
+// In-Class tutorials
 
+import { Component } from "@angular/core";
+
+//connects app html and css
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
